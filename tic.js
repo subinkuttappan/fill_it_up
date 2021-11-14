38044c13c162fbe2f0b14e22d97b134ca40ad94d
Subin Kuttappan Stellal Mary <subinkuttappan@gmail.com>
2021-11-14 16:33:02 +0530

    Update tic.js

--- tic.js
+++ tic.js
@@ -1,8 +1,30 @@
+var click_count = 0;
+
 function set_cyan(square_id) {
-	console.log("square_id: " + square_id);
-	for(id in square_id) {
-		console.log(square_id[id]);
-		switch(square_id[id]) {
+	
+	neighbors = [];
+	if(square_id % 5 != 1) {
+		neighbors.push(square_id - 1);
+	}
+
+	if(square_id % 5 != 0) {
+		neighbors.push(square_id + 1);
+	}
+
+	if(square_id > 5) {
+		neighbors.push(square_id - 5);
+	}
+
+	if(square_id <=20) {
+		neighbors.push(square_id + 5);
+	}
+	console.log("square_id: " + square_id + " neighbors: " + neighbors);
+	
+	for(id in neighbors) {
+		console.log(neighbors[id]);
+		var id = "b" + neighbors[id];
+		document.getElementById(id).style.backgroundColor = "#34ebd8";
+		/*switch(neighbors[id]) {
 			case 1 : document.getElementById("b1").style.backgroundColor = "#34ebd8";break;
 			case 2 : document.getElementById("b2").style.backgroundColor = "#34ebd8";break;
 			case 3 : document.getElementById("b3").style.backgroundColor = "#34ebd8";break;
@@ -12,35 +34,19 @@ function set_cyan(square_id) {
 			case 7 : document.getElementById("b7").style.backgroundColor = "#34ebd8";break;
 			case 8 : document.getElementById("b8").style.backgroundColor = "#34ebd8";break;
 			case 9 : document.getElementById("b9").style.backgroundColor = "#34ebd8";break;
-		}
+		}*/
 	}
-	//document.getElementById("b9").value = "0";
-}
-
-function set_grey(square_id) {
-	switch(square_id) {
-		case 1 : document.getElementById("b1").style.backgroundColor = "darkgrey";break;
-		case 2 : document.getElementById("b2").style.backgroundColor = "darkgrey";break;
-		case 3 : document.getElementById("b3").style.backgroundColor = "darkgrey";break;
-		case 4 : document.getElementById("b4").style.backgroundColor = "darkgrey";break;
-		case 5 : document.getElementById("b5").style.backgroundColor = "darkgrey";break;
-		case 6 : document.getElementById("b6").style.backgroundColor = "darkgrey";break;
-		case 7 : document.getElementById("b7").style.backgroundColor = "darkgrey";break;
-		case 8 : document.getElementById("b8").style.backgroundColor = "darkgrey";break;
-		case 9 : document.getElementById("b9").style.backgroundColor = "darkgrey";break;
-
-	}
-	console.log("set_grey : " + square_id);
+	click_count ++;
+	document.getElementById("moves").innerHTML = "Number of clicks : " + click_count;
 }
 
 function reset_squares() {
-	document.getElementById("b1").style.backgroundColor = "darkgrey";
-	document.getElementById("b2").style.backgroundColor = "darkgrey";
-	document.getElementById("b3").style.backgroundColor = "darkgrey";
-	document.getElementById("b4").style.backgroundColor = "darkgrey";
-	document.getElementById("b5").style.backgroundColor = "darkgrey";
-	document.getElementById("b6").style.backgroundColor = "darkgrey";
-	document.getElementById("b7").style.backgroundColor = "darkgrey";
-	document.getElementById("b8").style.backgroundColor = "darkgrey";
-	document.getElementById("b9").style.backgroundColor = "darkgrey";
+	for (i=1;i<=25;i++)
+	{
+		var id = "b" + i;
+		document.getElementById(id).style.backgroundColor = "darkgrey";
+	}
+	click_count=0;
+	document.getElementById("moves").innerHTML = "Number of clicks : 0";
+
 }
